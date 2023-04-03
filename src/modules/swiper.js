2828aac3a533be2d791907a64d69834998d91e6f
Denis Vydrytski <denis.vydrytski@gmail.com>
2023-04-03 19:17:08 +0300

    homepage without footer

---
+++ src/modules/swiper.js
@@ -0,0 +1,21 @@
+import Swiper, { Navigation, Pagination, Scrollbar } from "swiper";
+
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination, Scrollbar],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
+
+export default swiper;
